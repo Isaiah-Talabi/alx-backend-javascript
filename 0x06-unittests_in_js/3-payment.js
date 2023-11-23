f143486552e6b30d6a86c71def60e732687f27e9
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2023-11-23 00:15:05 +0000

    ijfojf

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,7 @@
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
